4fa4c6bb9c358edf55c937c2c6611006e769cc5d
Anmol Garg <22m2117@iitb.ac.in>
2023-01-30 22:52:54 +0530

    PARTIAL: added block and txn

---
+++ block.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from uuid import uuid4
+from txn import Transaction, CoinbaseTransaction
+from exceptions import BlockFullException
+from constants import *
+from enum import Enum
+
+class BlockType(Enum):
+    normal = "normal"
+    genesis = "genesis"
+
+class Block:
+    def __init__(self, prev_hash, miner_id):
+        self.id = uuid4()
+        self.type = BlockType.normal
+        self.timestamp = datetime.now().timestamp()
+        self.size = BLOCK_SIZE
+        self.txn_fees = 0
+        self.txns = []
+        self.prev_hash = prev_hash
+        self.miner_id = miner_id
+    
+    def add_txn(self, txn):
+        self.txn_fees += txn.txn_fees
+        if len(self.txns) < (self.size-1):
+            self.txns.append(txn)
+        else:
+            raise BlockFullException("Block is full")
+    
+    def prepare_block(self):
+        coinbase_txn = CoinbaseTransaction(self.miner_id,self.txn_fees)
+        self.txns[:0] = coinbase_txn
+    
+class GenesisBlock:
+    def __init__(self):
+        self.id = uuid4()
+        self.type = BlockType.genesis
+        self.timestamp = datetime.now().timestamp()
+        self.hash = '0'*256
+
+
+    
