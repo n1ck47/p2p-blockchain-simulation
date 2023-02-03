3e9654ce2607698f39dc6f4abdd6a69934994580
Anmol Garg <22m2117@iitb.ac.in>
2023-02-03 20:12:31 +0530

    added visualize.py

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
 from block import GenesisBlock,Block
+import queue
 
 class Blockchain:
     def __init__(self):
@@ -55,20 +56,22 @@ class Blockchain:
             heights = [self.allnodesinchain(child) for child in node.children]
             max_height, last_block = max(heights, key=lambda x: x[0])
             return (1 + max_height, [node] + last_block)
+        
     def display_chain(self):
         return [node.get_hash() for node in self.allnodesinchain(self.genesis)[1]]
+    
+    def get_blockchain_tree(self):
 
+        adjancency_list = {}
+        block = self.genesis
+        blocks_queue = queue.Queue()
+        blocks_queue.put(self.genesis)
 
-# blkchain=Blockchain()
-# b1=Block('0'*256)
-# b2=Block(b1.id)
-# b3=Block(b1.id)
-# b4=Block(b3.id)
-# b5=Block(b4.id)
-# blkchain.add_block(b1)
-# blkchain.add_block(b2)
-# blkchain.add_block(b3)
-# blkchain.add_block(b4)
-# print(b4.id)
-# print(blkchain.getlastblock().id)
-# print(blkchain.display_chain())
+        while(not blocks_queue.empty()):
+            block = blocks_queue.get()
+            adjancency_list[str(block.id)] = [ str(i.id) for i in block.children]
+
+            for bl in block.children:
+                blocks_queue.put(bl)
+        
+        return adjancency_list
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import simpy
 from node import Node
 from network import finalise_network
 import numpy as np
+from pathlib import Path
+from constants import *
 
 def initialize_nodes(n, z0, z1):
     network = list()
@@ -61,13 +63,29 @@ for node in Node.network:
 # env.process(Node.network[0].mine_block())
 # env.process(Node.network[1].mine_block())
 
-env.run(until=308118)
+env.run(until=508118)
 
 # for elm in Node.network:
 #     print(elm.id, len(elm.txn_pool))
 
-for node in Node.network:
+for node_i in range(len(Node.network)):
+    node = Node.network[node_i]
     print(node.id, len(node.blockchain.display_chain()))
+
+    Path(TREE_OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+    adj = node.blockchain.get_blockchain_tree()
+
+    with open(f'{TREE_OUTPUT_DIR}/{TREE_OUTPUT_FILE_PREFIX}{node_i}.txt', 'w') as f:
+        for i in adj:
+
+            pr_str = i
+            for j in adj[i]:
+                pr_str += f" {j}"
+            
+            pr_str += "\n"
+            
+            f.write(pr_str)
+
 
 
 # for node in Node.network:
