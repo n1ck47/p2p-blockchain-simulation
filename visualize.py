4f8bc7747be69b5da6353206f462ecd2336ea00b
Anmol Garg <22m2117@iitb.ac.in>
2023-02-05 11:25:55 +0530

    updated visualization

--- visualize.py
+++ visualize.py
@@ -6,6 +6,9 @@ from treelib import Tree
 from constants import *
 
 for fname in os.listdir(TREE_OUTPUT_DIR):
+    if TREE_OUTPUT_FILE_PREFIX in fname and "txt" not in fname:
+        continue
+
     tree = Tree()
     with open(f"{TREE_OUTPUT_DIR}/{fname}") as f:
         lines = [i.strip() for i in f.readlines()]
