68e5179bbc29c125fe102d85b4dd9cb6c651b009
Anmol Garg <22m2117@iitb.ac.in>
2023-03-21 14:36:10 +0530

    updated visualizer

--- visualize.py
+++ visualize.py
@@ -15,6 +15,9 @@ files.sort()
 for fname in files:             #Reading each file of a node and generating the blockchain for that node
     if TREE_OUTPUT_FILE_PREFIX in fname and "txt" not in fname:
         continue
+
+    BLOCK_NODE_MAPPING = {}
+
     print(f"Visualizing :: {fname}")
     tree = Tree()
     with open(f"{TREE_OUTPUT_DIR}/{fname}") as f:
@@ -23,16 +26,43 @@ for fname in files:             #Reading each file of a node and generating the 
     node_counter = 0
 
     root_node = lines[0].split()[0].split("::")[0]
+    BLOCK_NODE_MAPPING[root_node] = lines[0].split()[0].split("::")[2]
 
-    tree.create_node(str(node_counter), root_node)
+    tree.create_node(str(node_counter), root_node, data="-1")
     node_counter += 1
     for l in lines:
         nodes = l.split()
         for n in range(1, len(nodes)):
-            tree.create_node(str(node_counter), nodes[n].split("::")[0], parent=nodes[0].split("::")[0])
+            miner_id = nodes[n].split("::")[2]
+            block_id = nodes[n].split("::")[0]
+            BLOCK_NODE_MAPPING[block_id] = miner_id
+            tree.create_node(str(node_counter), block_id, parent=nodes[0].split("::")[0], data=str(miner_id))
             node_counter += 1
+    
 
     tree.to_graphviz("graph.txt")
+
+    # editing graph.txt to include color
+
+    with open("graph.txt") as f_graph:
+        lines = [i.strip() for i in f_graph.readlines()]
+    
+    updated_graph_txt = []
+
+    for line in lines:
+        if "label" in line and "shape" in line:
+            block_id = line.split('"')[1]
+            if BLOCK_NODE_MAPPING[block_id] == "0":
+                updated_line = line[:-1] + ', style="filled", fillcolor="red"]'
+                updated_graph_txt.append(updated_line)
+            else:
+                updated_graph_txt.append(line)
+        else:
+            updated_graph_txt.append(line)
+
+    with open("graph.txt", "w") as f_graph:
+        f_graph.writelines(updated_graph_txt)
+
     dot = pydot.graph_from_dot_file("graph.txt")
     os.remove("graph.txt")
 
